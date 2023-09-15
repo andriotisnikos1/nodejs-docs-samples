9d71299f9ff102c740172b0f51f12fe629b55738
Nikos Andriotis <67287051+andriotisnikos1@users.noreply.github.com>
2023-09-15 16:31:01 +0300

    Update quickstart.js

--- texttospeech/quickstart.js
+++ texttospeech/quickstart.js
@@ -20,7 +20,7 @@ function main() {
   const textToSpeech = require('@google-cloud/text-to-speech');
 
   // Import other required libraries
-  const fs = require('fs');
+  const fs = require('fs/promises');
   const util = require('util');
   // Creates a client
   const client = new textToSpeech.TextToSpeechClient();
@@ -40,8 +40,7 @@ function main() {
     // Performs the text-to-speech request
     const [response] = await client.synthesizeSpeech(request);
     // Write the binary audio content to a local file
-    const writeFile = util.promisify(fs.writeFile);
-    await writeFile('output.mp3', response.audioContent, 'binary');
+    await fs.writeFile('output.mp3', response.audioContent, 'binary');
     console.log('Audio content written to file: output.mp3');
   }
   quickStart();
